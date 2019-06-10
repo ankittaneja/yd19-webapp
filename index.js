47d6c0add3dde9ea615a866f70d79727dbde74a7
Ankit Taneja <ankittaneja090@gmail.com>
2019-06-10 15:00:12 +0200

    Updated html

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ router.get('/send', async (ctx,next) => {
 router.post('/send', async (ctx,next) => {
   // publish a message to a topic
   client.publish('hallo/amsterdam', 'This is YeurDreamin!!!', function() {
-    console.log('Message is published');
+    console.log('Message is published2');
   });
   ctx.status = 200;
   ctx.message = 'Post Successful';
